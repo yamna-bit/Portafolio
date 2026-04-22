c87a544024d40c013f9814ad87d00ec5b6b2d15b
Yamna Carrión <yamna.carrion@mail.udp.cl>
2026-04-21 22:25:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Portafolio Yamna listo');
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove",(e)=>{
+cursor.style.left = e.clientX + "px";
+cursor.style.top = e.clientY + "px";
+});
+
+const enterBtn = document.getElementById("enterBtn");
+const intro = document.getElementById("intro");
+const main = document.getElementById("main");
+
+enterBtn.addEventListener("click", ()=> {
+intro.style.display = "none";
+main.style.display = "block";
+});
