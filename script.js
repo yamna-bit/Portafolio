b9e87f568efdf54250005041d4be5dd68fdedae9
Yamna Carrión <yamna.carrion@mail.udp.cl>
2026-05-05 23:45:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// animación simple al hacer scroll
-window.addEventListener("scroll", () => {
-  const sections = document.querySelectorAll("section");
+// PORTAFOLIO INTERACTIVO - Yamna Carrión
 
-  sections.forEach(sec => {
-    const top = sec.getBoundingClientRect().top;
-
-    if (top < window.innerHeight - 100) {
-      sec.style.opacity = "1";
-      sec.style.transform = "translateY(0)";
-    }
-  });
-});
+let fontBold;
+let fontLight;
