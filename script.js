a72ce31cc93dca0336d9a508867f71364b30534d
Yamna Carrión <yamna.carrion@mail.udp.cl>
2026-05-05 20:25:16 -0400

    Implement page transition on link click
    Add page transition effect on link clicks.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Portafolio Yamna listo');
+// ===== TRANSICIÓN ENTRE PÁGINAS =====
+const links = document.querySelectorAll("a");
+const transition = document.querySelector(".transition");
+
+links.forEach(link => {
+  link.addEventListener("click", function(e){
+
+    // evita cambio inmediato
+    e.preventDefault();
+
+    const url = this.href;
+
+    // activa animación
+    transition.classList.add("active");
+
+    // espera y cambia de página
+    setTimeout(()=>{
+      window.location = url;
+    }, 500);
+  });
+});
