efbf33166d02e9527678be8ca16db719ef52b1d9
Yamna Carrión <yamna.carrion@mail.udp.cl>
2026-05-12 01:46:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Portafolio activo <3");
+document.addEventListener("DOMContentLoaded", () => {
+
+  document.body.classList.add("fade-in");
+
+  const links = document.querySelectorAll("a");
+
+  links.forEach(link => {
+    link.addEventListener("click", e => {
+
+      const href = link.getAttribute("href");
+
+      if (href && !href.startsWith("#")) {
+        e.preventDefault();
+
+        document.body.classList.remove("fade-in");
+        document.body.classList.add("fade-out");
+
+        setTimeout(() => {
+          window.location.href = href;
+        }, 700);
+      }
+    });
+  });
+
+});
